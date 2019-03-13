339319e6e6e3017b5bbb7bc6fcee3f68f6922a63
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-03-12 22:33:50 -0400

    html css  structure - box model

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,4 +1,17 @@
 class ConferencesController < ApplicationController
   def index
   end
+  def speakers
+  	
+  end
+  def schedule
+  	
+  end
+  def venue
+  	
+  end
+  def register
+  	
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root 'conferences#index'
+  #root 'conferences#index'
 
-  get 'conferences/index'
+  get 'index' => 'conferences#index'
+  get 'speakers' => 'conferences#speakers'
+  get 'schedule' => 'conferences#schedule'
+  get 'venue' => 'conferences#venue'
+  get 'register' => 'conferences#register'
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
